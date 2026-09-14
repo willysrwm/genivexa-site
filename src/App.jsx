016cf375a3fedff81ca8a0886cbfa100780d20c5
willysrwm <wilson.rand@gmail.com>
2026-09-14 07:28:33 +0300

    Add Article route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Footer from './components/Footer'
 import Home from './pages/Home'
 import Tools from './pages/Tools'
 import Blog from './pages/Blog'
+import Article from './pages/Article'
 import About from './pages/About'
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/outils" element={<Tools />} />
           <Route path="/blog" element={<Blog />} />
+          <Route path="/blog/:slug" element={<Article />} />
           <Route path="/a-propos" element={<About />} />
         </Routes>
       </main>
