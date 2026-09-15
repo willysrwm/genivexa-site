827aca1c124d0dacf409166eddf7f71ccf33d174
willysrwm <wilson.rand@gmail.com>
2026-09-15 17:40:15 +0300

    Make logo bigger in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
   return (
     <header className="bg-white shadow-sm sticky top-0 z-50">
       <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center h-20">
+        <div className="flex justify-between items-center h-24">
           {/* LOGO + TEXTE */}
           <Link to="/" className="flex items-center space-x-2 group">
            <img
