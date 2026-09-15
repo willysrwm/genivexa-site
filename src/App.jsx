8e94cc48526e543aa56d6e71adbe8c3156707f65
willysrwm <wilson.rand@gmail.com>
2026-09-15 16:13:48 +0300

    Connect ScrollToTop in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import ScrollToTop from './components/ScrollToTop'
 import Home from './pages/Home'
 import Tools from './pages/Tools'
 import Blog from './pages/Blog'
@@ -10,6 +11,7 @@ import About from './pages/About'
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
+      <ScrollToTop />
       <Header />
       <main className="flex-grow">
         <Routes>
